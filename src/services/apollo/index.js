277bfa434267508ab458466f9c6a293ddd87b508
Bilal Haidar <bhaidar@gmail.com>
2018-11-16 10:21:48 +0200

    Recipes-List is ready

--- src/services/apollo/index.js
+++ src/services/apollo/index.js
@@ -3,7 +3,7 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { ApolloLink, concat, split } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { authService } from '@/services/AuthService';
+import { authService } from '@/services/auth/AuthService';
 
 const httpLink = new HttpLink({
   uri: 'https://hasura-food-recipes.herokuapp.com/v1alpha1/graphql'
@@ -13,17 +13,17 @@ const authMiddleware = new ApolloLink((operation, forward) => {
   const token = authService.getAccessToken();
 
   // add the authorization to the headers
-  operation.setContext({ 
-    headers: { 
-      authorization: token ? `Bearer ${token}` : null 
-    } 
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : null
+    }
   });
 
   return forward(operation);
-})
+});
 
 // Create the apollo client
 export default new ApolloClient({
   link: concat(authMiddleware, httpLink),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache()
 });
--- src/store/modules/recipes.module.js
+++ src/store/modules/recipes.module.js
@@ -1,35 +1,76 @@
 import { router } from '@/router';
-import gqlClient from '@/apollo';
+import gqlClient from '@/services/apollo';
 import gql from 'graphql-tag';
 
 import { RECIPES_QUERY } from '@/queries';
 
 const state = {
-    all: []
+  all: []
 };
 
 const actions = {
-    async fetch({ commit }) {
-        const response = await gqlClient.query({
-          query: RECIPES_QUERY
-        });
-
-        commit("setRecipes", response.data.recipe);
-    }
+  async fetch({ commit }) {
+    commit('setRecipes', [
+      {
+        id: 1,
+        name: 'Recipe #1',
+        description:
+          'Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food ',
+        number_of_servings: 4,
+        calories_per_serving: 200
+      },
+      {
+        id: 2,
+        name: 'Recipe #2',
+        description:
+          'Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food ',
+        number_of_servings: 4,
+        calories_per_serving: 200
+      },
+      {
+        id: 3,
+        name: 'Recipe #3',
+        description:
+          'Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food',
+        number_of_servings: 4,
+        calories_per_serving: 200
+      },
+      {
+        id: 4,
+        name: 'Recipe #4',
+        description:
+          'Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food ',
+        number_of_servings: 4,
+        calories_per_serving: 200
+      },
+      {
+        id: 5,
+        name: 'Recipe #5',
+        description:
+          'Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food Recipe Food ',
+        number_of_servings: 4,
+        calories_per_serving: 200
+      }
+    ]);
+    // const response = await gqlClient.query({
+    //   query: RECIPES_QUERY
+    // });
+    //commit('setRecipes', response.data.recipe);
+  }
 };
 
 const mutations = {
-    setRecipes(state, recipes) {
-        state.all = recipes;
-    }
+  setRecipes(state, recipes) {
+    state.all = recipes;
+  }
 };
 
 const getters = {};
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    actions,
-    getters
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
 };
