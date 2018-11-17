307aaaf8db154fa5fc684c3fc5394066df680796
Bilal Haidar <bhaidar@gmail.com>
2018-11-17 02:08:19 +0200

    Add project info to Home page
    Fix UI issues on home page

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,26 +1,62 @@
 <template>
   <div>
-    <div class="text-center">
-      <h1>Welcome to Yummy Food!</h1>
-      <p>
-        This is a sample application accompanying an article on Hasura GraphQL
-        Engine. The article is published in three parts.
-      </p>
-    </div>
-    <ul class="parts">
-      <li><a href="">Part 1</a></li>
-      <li><a href="">Part 2</a></li>
-      <li><a href="">Part 3</a></li>
-    </ul>
-    <div class="techs">
-      <h3 class="text-center">Technologies used in this sample application:</h3>
-      <ul>
-        <li><a href="">Vue.js</a></li>
-        <li>Authentication/Authorization with <a href="">auth0</a></li>
-        <li><a href="">Hasura GraphQL Engine</a></li>
-        <li><a href="">Apollo Client for Vue.js</a></li>
-        <li><a href="">Heroku</a> to host Hasura Engine</li>
-      </ul>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="header section">
+            <h2>Welcome to Yummy Food!</h2>
+            <p>
+              This is a sample application accompanying an article on Hasura GraphQL
+              Engine. The article is published in three parts.
+            </p>
+            <div class="articles">
+              <ol>
+                <li><a href="">Part 1</a></li>
+                <li><a href="">Part 2</a></li>
+                <li><a href="">Part 3</a></li>
+              </ol>
+            </div> 
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section higlight">
+            <h2>Technologies used</h2>
+            <p>Here's a lsit of the major fameworks used tin his sample</p>
+            <ol>
+              <li><a href="">Vue.js</a></li>
+              <li><a href="">Authentication/Authorization</a> with <a href="">auth0</a></li>
+              <li><a href="">Hasura GraphQL Engine</a></li>
+              <li><a href="">Apollo Client for Vue.js</a></li>
+              <li><a href="">Heroku</a> to host Hasura Engine</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+       <div class="row">
+        <div class="col-md-12">
+          <div class="section">
+            <h2>Enhancements</h2>
+            <p>This application is far from being complete. It's just a playground to show how we can mix auth0, Vue.js, Apollo Client GraphQL and Hasura GraphQL Engine all together. Yet many things could be added and enhanced. Hopefully I will be working on adding the features below and of course welcoming any assistance with that :-)</p>
+            <ol>
+              <li>Allow the user to delete a Recipe</li>
+              <li>Allow the user to create a new Recipe</li>
+              <li>Allow the user to list all Ingredients</li>
+              <li>Allow the user to create an Ingredient</li>
+              <li>Allow the user to edit  an Ingredient</li>
+              <li>Allow the user to delete an Ingredient</li>
+              <li>Allow the user to list all Food Categories</li>
+              <li>Allow the user to create a Food Category</li>
+              <li>Allow the user to edit a Food Category</li>
+              <li>Allow the user to delete a Food Category</li>
+              <li>Add form validation to all forms above, including editing a Recipe</li>
+              <li>Enhance the UI experience</li>
+              <li>And many other things</li>
+            </ol>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,62 +65,35 @@
 // @ is an alias to /src
 
 export default {
-  name: "home"
+  name: 'home'
 };
 </script>
 
 <style scoped>
-.parts {
-  display: flex;
-  justify-content: space-around;
+ol {
   padding: 0;
-  margin: 20px 0 0 0;
+  margin: 0 0 0 20px;
 }
-
-.parts li {
-  list-style-type: none;
+li {
+  margin-top: 5px;
 }
-
-.parts li a {
-  padding: 10px;
+ol li a {
+  padding: 5px;
   color: #333;
 }
-.parts li a:hover {
+ol li a:hover {
   text-decoration: none;
   border-bottom: solid 2px #333;
 }
-
-.techs {
-  margin: 30px auto;
+.section {
+  margin: 0;
+  margin-top: 20px;
+  padding: 20px;
 }
-
-.techs h3 {
-  font-size: 1.25rem;
+.header {
+  margin-top: 0;
 }
-
-.techs > ul {
-  display: block;
-}
-
-@media screen and (max-width: 768px) {
-  .parts {
-    display: block;
-  }
-
-  .parts li {
-    display: block;
-    padding: 10px;
-  }
-
-  .techs {
-    width: 100%;
-    padding: 0;
-  }
-}
-
-@media screen and (min-width: 769px) {
-  .techs {
-    width: 80%;
-  }
+.higlight {
+  background-color: #eee;
 }
 </style>
